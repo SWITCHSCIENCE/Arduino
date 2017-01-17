d99b1b5cba5f72dca91612db5c877a9445aefeaa
Shigeru KANEMOTO <sgk@switch-science.com>
2017-01-17 23:43:49 +0900

    Add Switch Science ESPr series boards

---
+++ variants/espr/pins_arduino.h
@@ -0,0 +1,20 @@
+#ifndef Pins_Arduino_h
+#define Pins_Arduino_h
+
+#include "../generic/common.h"
+
+static const uint8_t SDA = 4;
+static const uint8_t SCL = 5;
+
+static const uint8_t IO0 = 0;
+static const uint8_t IO2 = 2;
+static const uint8_t IO4 = 4;
+static const uint8_t IO5 = 5;
+static const uint8_t IO12 = 12;
+static const uint8_t IO13 = 13;
+static const uint8_t IO14 = 14;
+static const uint8_t IO15 = 15;
+static const uint8_t IO16 = 16;
+static const uint8_t TOUT = 17;
+
+#endif /* Pins_Arduino_h */
